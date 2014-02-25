fd874290e4fa9bf93fff5b4de22ccdc0b454cb96
balser <balser.brian@gmail.com>
2014-02-25 17:20:30 -0500

    Remove apt/yum refs

--- Berksfile
+++ Berksfile
@@ -1,7 +1,6 @@
 #site :opscode
 source 'https://api.berkshelf.com'
 
-cookbook 'apt', git: 'https://github.com/opscode-cookbooks/apt.git'
 cookbook 'stackdriver', git: 'git@github.com:tablexi/chef-stackdriver.git'
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,5 @@ description      'Installs/Configures np-chef-stackdriver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-#depends 'yum'
 depends 'apt'
 depends 'stackdriver'
