d7465944d94fd5ae1d143304c8f945d525297360
balser <balser.brian@gmail.com>
2014-02-25 22:45:23 -0500

    remove apt depends

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,4 @@ description      'Installs/Configures np-chef-stackdriver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apt'
 depends 'stackdriver'
