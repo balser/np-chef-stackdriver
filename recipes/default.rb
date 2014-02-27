f0fcfd2d76cbea7c6880d8befce650e3ebda624e
balser <balser.brian@gmail.com>
2014-02-26 21:57:59 -0500

    Add nginx plugin

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,4 +11,8 @@
 secure_stackdriver = Chef::EncryptedDataBagItem.load('secure', 'stackdriver')
 node.set[:stackdriver][:api_key] = secure_stackdriver['api_key']
 
+node.set[:stackdriver][:plugins][:nginx][:enable] = true
+node.set[:stackdriver][:plugins][:nginx][:url]    = 'http://127.0.0.1/nginx_status'
+
 include_recipe 'stackdriver'
+include_recipe 'stackdriver::plugins'
